22c5e9a9e8f0f3283511a8d6292b63b000b22fc7
Sakakura <njb-C18256@sist.ac.jp>
2019-02-14 20:54:32 +0900

    update and drop Implemented

--- src/app.js
+++ src/app.js
@@ -6,47 +6,84 @@ const http_1 = require("http");
 const socket_io = require("socket.io");
 const dbClasses_1 = require("./dbClasses");
 const app = express();
-app.use(express.static(__dirname + "/"));
+app.use(express.static('public'));
 const server = http_1.createServer(app);
-// server.listen(80);
+server.listen(80);
 const wss = socket_io(8080);
 const conn = promise_mysql_1.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    database: "bookrentalsystem"
+    host: '127.0.0.1',
+    user: 'root',
+    database: 'bookrentalsystem'
 }).then((connection) => {
-    wss.on("connection", ws => {
-        console.log("connected");
-        ws.on("get", () => {
+    wss.on('connection', ws => {
+        console.log('connected');
+        ws.on('get', () => {
             console.log('get');
             (async () => {
                 try {
                     let database = new dbClasses_1.DataBase();
-                    database.books = await connection.query("select * from bookrentalsystem.books");
-                    database.members = await connection.query("select * from bookrentalsystem.members");
-                    database.bookDetails = await connection.query("select * from bookrentalsystem.bookDetails");
-                    database.histories = await connection.query("select * from bookrentalsystem.histories");
-                    ws.emit("set", database);
+                    database.books = await connection.query('select * from bookrentalsystem.books');
+                    database.members = await connection.query('select * from bookrentalsystem.members');
+                    database.bookDetails = await connection.query('select * from bookrentalsystem.bookDetails');
+                    database.histories = await connection.query('select * from bookrentalsystem.histories');
+                    ws.emit('set', database);
                 }
                 catch (e) {
                     console.error(e);
                 }
             })();
         });
-        ws.on("append", (dataBase) => {
-            console.log(Object.keys(dbClasses_1.DataBase));
-            Object.keys(dataBase).forEach((tablename) => {
+        ws.on('append', async (dataBase) => {
+            console.log('append');
+            for (let tablename of Object.keys(dataBase)) {
                 if (typeof dataBase[tablename] === 'object' && dataBase[tablename][0]) {
-                    console.log(tablename);
-                    dataBase[tablename].forEach((rows) => {
-                        connection.query(`insert into bookrentalsystem.${tablename} set ?`, rows).catch(err => {
-                            ws.emit("err", err);
-                            throw err;
+                    for (let rows of dataBase[tablename]) {
+                        await connection.query(`insert into bookrentalsystem.${tablename} set ?`, rows).catch(err => {
+                            ws.emit('err', err);
+                            console.error(err);
                         });
-                    });
+                    }
                 }
-            });
-            wss.emit("append", dataBase);
+            }
+            wss.emit('append', dataBase);
+        });
+        ws.on('update', async (dataBase) => {
+            console.log('update');
+            if (dataBase.books[0]) {
+                for (let row of dataBase.books) {
+                    await connection.query(`update bookrentalsystem.books set ? where isbn = ?`, [row, row.isbn]);
+                }
+            }
+            if (dataBase.bookDetails[0]) {
+                for (let row of dataBase.bookDetails) {
+                    await connection.query(`update bookrentalsystem.bookDetails set ? where isbn = ? and serial = ?`, [row, row.isbn, row.serial]);
+                }
+            }
+            if (dataBase.members[0]) {
+                for (let row of dataBase.members) {
+                    await connection.query(`update bookrentalsystem.members set ? where id = ?`, [row, row.id]);
+                }
+            }
+            wss.emit('update', dataBase);
+        });
+        ws.on('drop', async (dataBase) => {
+            console.log('drop');
+            if (dataBase.books[0]) {
+                for (let row of dataBase.books) {
+                    await connection.query(`delete from bookrentalsystem.books where isbn = ?`, row.isbn);
+                }
+            }
+            if (dataBase.bookDetails[0]) {
+                for (let row of dataBase.bookDetails) {
+                    await connection.query(`delete from bookrentalsystem.bookDetails where isbn = ? and serial = ?`, [row.isbn, row.serial]);
+                }
+            }
+            if (dataBase.members[0]) {
+                for (let row of dataBase.members) {
+                    await connection.query(`delete from bookrentalsystem.members where id = ?`, row.id);
+                }
+            }
+            wss.emit('drop', dataBase);
         });
     });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,93 @@
-import express = require("express");
-import { createConnection, Connection } from "promise-mysql";
-import { createServer } from "http";
-import socket_io = require("socket.io");
+import express = require('express');
+import { createConnection, Connection } from 'promise-mysql';
+import { createServer } from 'http';
+import socket_io = require('socket.io');
 import {
   BookDetail,
   Book,
   RentHistory,
   Member,
   DataBase
-} from "./dbClasses";
+} from './dbClasses';
 
 const app = express();
-app.use(express.static(__dirname + "/"));
+app.use(express.static('public'));
 const server = createServer(app);
-// server.listen(80);
+server.listen(80);
 const wss = socket_io(8080);
 const conn = createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  database: "bookrentalsystem"
+  host: '127.0.0.1',
+  user: 'root',
+  database: 'bookrentalsystem'
 }).then((connection) => {
-  wss.on("connection", ws => {
-    console.log("connected");
-    ws.on("get", () => {
+  wss.on('connection', ws => {
+    console.log('connected');
+    ws.on('get', () => {
       console.log('get');
-      (async ()=>{
+      (async () => {
         try {
           let database: DataBase = new DataBase();
-          database.books = await connection.query("select * from bookrentalsystem.books");
-          database.members = await connection.query("select * from bookrentalsystem.members");
-          database.bookDetails = await connection.query("select * from bookrentalsystem.bookDetails");
-          database.histories = await connection.query("select * from bookrentalsystem.histories");
-          ws.emit("set", database);
-        } catch(e) {
+          database.books = await connection.query('select * from bookrentalsystem.books');
+          database.members = await connection.query('select * from bookrentalsystem.members');
+          database.bookDetails = await connection.query('select * from bookrentalsystem.bookDetails');
+          database.histories = await connection.query('select * from bookrentalsystem.histories');
+          ws.emit('set', database);
+        } catch (e) {
           console.error(e);
         }
       })();
     });
-    ws.on("append", (dataBase: DataBase) => {
-      console.log(Object.keys(DataBase));
-      Object.keys(dataBase).forEach((tablename) => {
+    ws.on('append', async (dataBase: DataBase) => {
+      console.log('append');
+      for (let tablename of Object.keys(dataBase)) {
         if (typeof dataBase[tablename] === 'object' && dataBase[tablename][0]) {
-          console.log(tablename);
-          dataBase[tablename].forEach((rows: Book | BookDetail | Member | RentHistory) => {
-            connection.query(`insert into bookrentalsystem.${tablename} set ?`, rows).catch(err => {
-              ws.emit("err", err);
-              throw err;
+          for (let rows of dataBase[tablename]) {
+            await connection.query(`insert into bookrentalsystem.${tablename} set ?`, rows).catch(err => {
+              ws.emit('err', err);
+              console.error(err);
             });
-          });
+          }
         }
-      });
-      wss.emit("append", dataBase);
+      }
+      wss.emit('append', dataBase);
+    });
+    ws.on('update', async (dataBase: DataBase) => {
+      console.log('update');
+      if (dataBase.books[0]) {
+        for (let row of dataBase.books) {
+          await connection.query(`update bookrentalsystem.books set ? where isbn = ?`, [row, row.isbn]);
+        }
+      }
+      if (dataBase.bookDetails[0]) {
+        for (let row of dataBase.bookDetails) {
+          await connection.query(`update bookrentalsystem.bookDetails set ? where isbn = ? and serial = ?`, [row, row.isbn, row.serial]);
+        }
+      }
+      if (dataBase.members[0]) {
+        for (let row of dataBase.members) {
+          await connection.query(`update bookrentalsystem.members set ? where id = ?`, [row, row.id]);
+        }
+      }
+      wss.emit('update', dataBase);
+    });
+    ws.on('drop', async (dataBase: DataBase) => {
+      console.log('drop');
+      if (dataBase.books[0]) {
+        for (let row of dataBase.books) {
+          await connection.query(`delete from bookrentalsystem.books where isbn = ?`, row.isbn);
+        }
+      }
+      if (dataBase.bookDetails[0]) {
+        for (let row of dataBase.bookDetails) {
+          await connection.query(`delete from bookrentalsystem.bookDetails where isbn = ? and serial = ?`, [row.isbn, row.serial]);
+        }
+      }
+      if (dataBase.members[0]) {
+        for (let row of dataBase.members) {
+          await connection.query(`delete from bookrentalsystem.members where id = ?`, row.id);
+        }
+      }
+      wss.emit('drop', dataBase);
     });
   });
 });
